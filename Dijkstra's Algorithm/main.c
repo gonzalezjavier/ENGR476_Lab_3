c46ad7a55dc3b8d1613f94bd5aefb11317430c41
Javier Gonzalez <gonzalezjavier925@gmail.com>
2020-11-11 18:14:59 -0800

    Initial Commit

---
+++ Dijkstra's Algorithm/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Dijkstra's Algorithm
+//
+//  Created by Javier Gonzalez on 11/11/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
